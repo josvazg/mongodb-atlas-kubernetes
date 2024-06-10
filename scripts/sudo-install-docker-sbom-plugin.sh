0ec36fa94df8e2e207b45b20000f9e31fa73672c
jose.vazquez <jose.vazquez@mongodb.com>
2024-06-10 18:56:14 +0200

    Use the docker sbom plugin directly

--- scripts/sudo-install-docker-sbom-plugin.sh
+++ scripts/sudo-install-docker-sbom-plugin.sh
@@ -6,7 +6,8 @@ version=${DOCKER_SBOM_PLUGIN_VERSION:-latest}
 os=${OS:-linux}
 arch=${ARCH:-amd64}
 target=$TMPDIR/sbom-cli-plugin.tgz
-plugin_dir="$HOME/.docker/cli-plugins/"
+docker_path=$(which docker)
+docker_dir=$(dirname "${docker_path}")
 
 download_url_base=https://github.com/docker/sbom-cli-plugin/releases/download
 url="${download_url_base}/v${version}/sbom-cli-plugin_${version}_${os}_${arch}.tar.gz"
@@ -16,4 +17,4 @@ pushd "${TMPDIR}"
 tar zxvf "${target}" docker-sbom
 chmod +x docker-sbom
 popd
-cp "${TMPDIR}/docker-sbom" "${plugin_dir}"
+sudo cp "${TMPDIR}/docker-sbom" "${docker_dir}"
